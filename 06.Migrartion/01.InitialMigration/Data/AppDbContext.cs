ec5994d3819615269195db90b93e3f729dfd686f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-24 13:10:23 +0200

    Migrations

---
+++ 06.Migrartion/01.InitialMigration/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Data.Config;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new CourseConfiguration());         // Not best practice
+            //modelBuilder.ApplyConfiguration(new InstructorConfiguration());     // Not best practice
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CourseConfiguration).Assembly);     // best practice
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetSection("ConnectionStrings:DefaultString").Value!;
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ 06.Migrartion/01.InitialMigration/Data/Config/InstructorConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Data.Config
+{
+    public class InstructorConfiguration : IEntityTypeConfiguration<Instructor>
+    {
+        public void Configure(EntityTypeBuilder<Instructor> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).ValueGeneratedNever();
+
+            builder.Property(i => i.Name)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.ToTable("Instructors");
+
+            builder.HasData(LoadCourses());
+        }
+
+        private static List<Instructor> LoadCourses()
+        {
+            //--Inserting data for Instructors
+            //INSERT INTO Instructors(Id, Name) VALUES(1, 'Ahmed Abdullah');
+            //INSERT INTO Instructors(Id, Name) VALUES(2, 'Yasmeen Mohammed');
+            //INSERT INTO Instructors(Id, Name) VALUES(3, 'Khalid Hassan');
+            //INSERT INTO Instructors(Id, Name) VALUES(4, 'Nadia Ali');
+            //INSERT INTO Instructors(Id, Name) VALUES(5, 'Omar Ibrahim');
+
+            return new List<Instructor>
+            {
+                new Instructor { Id = 1, Name = "Ahmed Abdullah" },
+                new Instructor { Id = 2, Name = "Yasmeen Mohammed" },
+                new Instructor { Id = 3, Name = "Khalid Hassan" },
+                new Instructor { Id = 4, Name = "Nadia Ali" },
+                new Instructor { Id = 5, Name = "Omar Ibrahim" }
+            };
+        }
+    }
+}
